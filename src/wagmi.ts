177fc1af1967cb2a5a5a493a69368749c6de6c7a
Skythrill256 <anjishnuganguly773@gmail.com>
2024-11-12 21:00:31 +0530

    swap-component

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -7,10 +7,14 @@ import {
   polygon,
   sepolia,
 } from 'wagmi/chains';
+const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
+if (!projectId) {
+  throw new Error('Environment variable NEXT_PUBLIC_WC_PROJECT_ID is not set.');
+}
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
-  projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
+  projectId: projectId as string,
   chains: [
     mainnet,
     polygon,
